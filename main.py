1b6cf293ae55dc8bdc5fd52f6dc36b6041ae2c65
Justin Carter <jscarter@umich.edu>
2017-07-02 16:39:12 -0700

    Did a ton of stuff

--- main.py
+++ main.py
@@ -5,40 +5,34 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
+
+errors = ()
+
 @app.route("/", methods=['POST'])
 def login():
     username = request.form['username']
     password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
-    error_list = ()
 
-    if username == '' or if len(username) < 3 or if len(username) > 20 or if ' ' in username:
-        append.error_list("Please provide a valid username")
-        else:
-            append.eror_list(" ")
-    elif password == '' or if len(password) < 3 or if len(password) > 20 or if ' ' in password:
-        append.error_list("Please provide a valid password")
-        else:
-            append.eror_list(" ")
-    elif verify != password:
-        append.error_list("Does not match you password")
-        else:
-            append.eror_list(" ")
-    elif email == '' or if len(email) < 3 or if len(email) > 20 or if ' ' in email or email.count('@') != 1 or email.count('.') !=1:
-        append.error_list("Please provide a valid email")
-        else:
-            append.eror_list(" ")
+    # if username == '' or if len(username) < 3 or if len(username) > 20 or if ' ' in username:
+    #     errors.insert(0,"Please provide a valid username")
+    # if password == '' or if len(password) < 3 or if len(password) > 20 or if ' ' in password:
+    #     errors.insert(1,"Please provide a valid password")
+    # if verify != password:
+    #     errors.insert(2,"Does not match your password")
+    # if email == '' or if len(email) < 3 or if len(email) > 20 or if ' ' in email or email.count('@') != 1 or email.count('.') !=1:
+    #     errors.insert(3,"Please provide a valid email")
 
-    if error_list = ( , , , )
-        #redirect to successful page
-        else:
-            #render form with errors
-    
+    if any(errors):
+        return render_template('form.html', Name_Err=errors[0], Pass_Err=errors[1], Ver_Err=errors[2], Mail_Err=errors[3])
+    else:
+        return render_template("success.html", username=username)
+
 @app.route("/")
 def index():
     '''renders the inital signup form'''
-    return render_template('form.html')
+    return render_template('form.html', Name_Err='', Pass_Err='', Ver_Err='', Mail_Err='')
 
 
 
