b92661ed347877a1dfc1453f2ee4356c3fcb62dd
Justin Carter <jscarter@umich.edu>
2017-07-02 13:27:10 -0700

    Added list tracking for errors

--- main.py
+++ main.py
@@ -11,20 +11,30 @@ def login():
     password = request.form['password']
     verify = request.form['verify']
     email = request.form['email']
+    error_list = ()
 
-    if username == '' or if len(username)<3 or if len(username)>20 or if ' ' in username:
-        error = "Please provide a valid username"
+    if username == '' or if len(username) < 3 or if len(username) > 20 or if ' ' in username:
+        append.error_list("Please provide a valid username")
         else:
-    if password == '' or if len(password)<3 or if len(password)>20 or if ' ' in password:
-        error = "Please provide a valid password"
+            append.eror_list(" ")
+    elif password == '' or if len(password) < 3 or if len(password) > 20 or if ' ' in password:
+        append.error_list("Please provide a valid password")
         else:
-    if verify != password:
-        error = "Does not match you password" 
+            append.eror_list(" ")
+    elif verify != password:
+        append.error_list("Does not match you password")
         else:
-    if email == '' or if len(email)<3 or if len(email)>20 or if ' ' in email or email.count('@') != 1 or email.count('.') !=1:
-        error = "Please provide a valid email"
+            append.eror_list(" ")
+    elif email == '' or if len(email) < 3 or if len(email) > 20 or if ' ' in email or email.count('@') != 1 or email.count('.') !=1:
+        append.error_list("Please provide a valid email")
         else:
+            append.eror_list(" ")
 
+    if error_list = ( , , , )
+        #redirect to successful page
+        else:
+            #render form with errors
+    
 @app.route("/")
 def index():
     '''renders the inital signup form'''
